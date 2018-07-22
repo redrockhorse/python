053d81cf8fc4b906e0bb64b429cd32d7b4453c09
Alex Mahone <kkkkibj@163.com>
2018-07-22 12:12:38 +0800

    awp

--- palmgo3.5/awp_data_handle.py
+++ palmgo3.5/awp_data_handle.py
@@ -6,6 +6,7 @@
 
 import json
 import math
+import requests
 def gps_coord_02tobd(gps_cx_02, gps_cy_02):
     x_pi = 3.14159265358979324 * 3000.0 / 180.0
     z = math.sqrt(gps_cx_02 * gps_cx_02 + gps_cy_02 * gps_cy_02) + 0.00002 * math.sin(gps_cy_02 * x_pi)
@@ -16,6 +17,7 @@ def gps_coord_02tobd(gps_cx_02, gps_cy_02):
 
 result={}
 result['data']=[]
+extendfile = open('/Users/hongyanma/Desktop/awpdata/awp_2017_area.txt','w')
 with open('/Users/hongyanma/Desktop/awpdata/data.txt','r') as inputfile:
     i=0
     for line in inputfile:
@@ -25,10 +27,21 @@ with open('/Users/hongyanma/Desktop/awpdata/data.txt','r') as inputfile:
             arr=line.replace('\n','').split(',')
             coords = gps_coord_02tobd(float(arr[4]), float(arr[5]))
             #result['data'].append([arr[4],arr[5]])
-            result['data'].append(['%.6f' % coords[0], '%.6f' % coords[1]])
-        i+=1
+            #result['data'].append(['%.6f' % coords[0], '%.6f' % coords[1]])
+            r = requests.get(url='http://api.map.baidu.com/geocoder/v2/',
+                             params={'location': '%.6f' % coords[1]+','+'%.6f' % coords[0], 'ak': '1XjLLEhZhQNUzd93EjU5nOGQ', 'output': 'json'})
 
+            result = r.json()
+            #city = result['result']['addressComponent']['city']
+            #addrs = result['result']['addressComponent']
+            extendfile.write(line.replace('\n','').join(result['result']['addressComponent']['province']).join(',').join(result['result']['addressComponent']['city']).join(',').join(result['result']['addressComponent']['district']).join(',').join(result['result']['addressComponent']['town']).join(',').join(result['result']['addressComponent']['street']).join('\n'))
+            #print(addrs)
+        i+=1
+extendfile.close()
+'''
 with open('/Users/hongyanma/Desktop/awpdata/awp_2017_gps.json','w') as outputfile:
     json.dump(result,outputfile)
+
+'''
 
 
